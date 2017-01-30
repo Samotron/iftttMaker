1215d3418c08ab436e69d953dc9a57bf618dd2bf
Sam Rogers <jasarogers@gmail.com>
2017-01-30 20:30:43 +0000

    Update iftttMaker.py

--- iftttMaker.py
+++ iftttMaker.py
@@ -3,6 +3,13 @@
 import requests
 
 def iftttMaker(event, key, value1="", value2="", value3=""):
+    """
+    Input your event and key as specified on the IFTTT Maker Channel.
+    
+    If your applet allows, three custom values can be included.
+    
+    More info here:
+    """
     payload = r"{ 'value1 = '" + value1 + "', value2 = '" + value2 + "', value3 = '"+ value3 +"'}"
     requests.post(r"https://maker.ifttt.com/trigger/" + event +"/with/key/" + key, data=payload)
 
